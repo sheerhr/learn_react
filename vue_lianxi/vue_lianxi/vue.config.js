9c4a86d19ee3eb0922612ddbbc4525505c747ffc
sheerhr <597982448@qq.com>
2021-03-03 11:22:56 +0800

    master

---
+++ vue_lianxi/vue_lianxi/vue.config.js
@@ -0,0 +1,11 @@
+const port = 8002;
+
+module.exports = {
+    publicPath : "/bestPractice",
+    devServer : {
+        port
+    },
+    configureWebpack : {
+        name :'vue项目最佳实践'
+    }
+}
---
+++ vue_yuxi/vue_yuxi/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div v-if="isLogin">
+      <h1>
+        美军空袭叙利亚了
+      </h1>
+    </div>
+    <div>
+      <button @click="toggle" type="middle">
+        {{showValue}}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data() {
+      return {
+          // isLogin : this.$store.state.isLogin
+          showValue:"登录"
+      }
+  },
+  name: 'Home',
+  components: {
+    // HelloWorld
+  },
+  computed: mapState({
+      // 箭头函数可使代码更简练
+      isLogin: state => console.log(state),
+
+      // 传字符串参数 'count' 等同于 `state => state.count`
+      countAlias: 'count',
+
+      // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+      countPlusLocalState (state) {
+        return state.count + this.localCount
+      }
+  }),
+  methods: {
+    toggle (state) {
+      if(this.showValue == "登录") {
+        this.showValue = "登出";
+        this.$store.commit('login');
+      }else {
+        this.showValue = "登录";
+        this.$store.commit('logOut');
+      }
+    }
+  },
+}
+</script>
